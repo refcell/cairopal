ead8ef69d3b73b072748b865ea384bbbe1db7807
Alucard <ladonseth1@gmail.com>
2021-12-27 09:48:13 -0700

    nextjs typescript chakra template

---
+++ next-seo.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next-seo').DefaultSeoProps} */
+const defaultSEOConfig = {
+  title: "cairopal",
+  titleTemplate: "%s | cairopal",
+  defaultTitle: "cairopal",
+  description: "Flexible Starknet Dapp Template",
+  canonical: "https://nextarter-chakra.sznm.dev",
+  openGraph: {
+    url: "https://nextarter-chakra.sznm.dev",
+    title: "cairopal",
+    description: "Flexible Starknet Dapp Template",
+    images: [
+      {
+        url: "https://cairopal.xyz/cairopal.png",
+        alt: "cairopal cairopal",
+      },
+    ],
+    site_name: "cairopal",
+  },
+  twitter: {
+    handle: "@a5f9t4",
+    cardType: "summary_large_image",
+  },
+};
+
+export default defaultSEOConfig;
---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import { Box } from '@chakra-ui/react';
+
+import CTASection from 'components/samples/CTASection';
+import SomeImage from 'components/samples/SomeImage';
+import SomeText from 'components/samples/SomeText';
+
+const Home = () => {
+	return (
+		<Box mb={8} w="full">
+			<SomeText />
+			<SomeImage />
+			<CTASection />
+		</Box>
+	);
+};
+
+export default Home;
